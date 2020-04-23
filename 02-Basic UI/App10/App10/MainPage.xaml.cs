84a8155b9ac3d7b152a0d01bda9cd88e57ca8e4c
Hadi Fooladi Talari <hadi.f7@gmail.com>
2020-04-23 13:50:27 -0400

    02-Basic UI

---
+++ 02-Basic UI/App10/App10/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App10
+{
+	// Learn more about making custom code visible in the Xamarin.Forms previewer
+	// by visiting https://aka.ms/xamarinforms-previewer
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+			//var SL = new StackLayout();
+			//var lbl = new Label
+			//{
+			//	Text = "Xamarin.Forms (C#)",
+			//	TextColor = Color.Red,
+			//	HorizontalOptions = LayoutOptions.Center,
+			//	VerticalOptions = LayoutOptions.CenterAndExpand
+			//};
+
+			//SL.Children.Add(lbl);
+
+			//Content = SL;
+		}
+
+		private void bAdd_Clicked(object sender, EventArgs e)
+		{
+			float
+				Num1 = float.Parse(tbNumber1.Text),
+				Num2 = float.Parse(tbNumber2.Text),
+				Result = Num1 + Num2;
+
+			lblResult.Text = $"{Num1} + {Num2} = {Result}";
+		}
+	}
+}
