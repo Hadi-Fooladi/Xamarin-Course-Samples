6aa80115fad08c9006164b0f427d757d7346c5ea
Hadi Fooladi Talari <hadi.f7@gmail.com>
2020-04-23 13:40:47 -0400

    Basic OOP Sample

---
+++ OOP/ConsoleApp9/Base.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp9
+{
+	class Base
+	{
+		public Base(int value)
+		{
+			a = value;
+			Counter++;
+		}
+
+		public Base() : this(5)
+		{
+		}
+
+		~Base()
+		{
+			Counter--;
+		}
+
+		private int a;
+		public int b;
+
+		public static int Counter = 0;
+
+		public static void ShowCount()
+		{
+			Console.WriteLine(Counter);
+		}
+
+		public void Set(int value = 63) => a = value;
+
+		public int Get()
+		{
+			return a;
+		}
+
+		public int Get2() => a;
+
+		public void Write(string What)
+		{
+			Console.WriteLine(What + a);
+		}
+
+		public virtual void WriteVirtual()
+		{
+			Console.WriteLine("Base" + a);
+		}
+	}
+
+	class Child : Base
+	{
+		public Child() : base(78)
+		{
+
+		}
+
+		public new void Write(string What)
+		{
+			Console.WriteLine("-------------");
+			base.Write(What);
+			Console.WriteLine("-------------");
+		}
+
+		public override void WriteVirtual()
+		{
+			Console.WriteLine("Child" + base.Get());
+		}
+	}
+}
---
+++ OOP/ConsoleApp9/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Base B = new Base();
+			//var C = new Base(7);
+			//B.Write("Hello!");
+			//B.Set(15);
+			//B.Write("Hello!");
+			//B.Set();
+			//B.Write("Hello!");
+
+			//C.Write("C");
+
+			Base.ShowCount();
+
+			var obj = new Child();
+			//obj.b = 1;
+			//Console.WriteLine(obj.Get());
+			//obj.Write("Child");
+
+			foo(obj);
+
+			Base.ShowCount();
+
+			var B = new Base();
+
+			foo(B);
+
+			Base.ShowCount();
+
+			Console.ReadKey();
+		}
+
+		static void foo(Base B)
+		{
+			B.Write("foo");
+			B.WriteVirtual();
+		}
+	}
+}
